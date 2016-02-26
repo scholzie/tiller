c82f610a53bee0c9d37030ef3e48d9bf9a903fbe
Chris Scholz <chris.scholz@blueapron.com>
2016-02-26 17:16:47 -0500

    Modified outputs.tf
    Summary: Added more outputs to outputs.tf
    
    Test Plan:
    Check that new outputs are present when running terraform apply.
    Sample Output:
    Outputs:
    
      asg.availability_zones        = us-east-1a,us-east-1c,us-east-1d,us-east-1e
      asg.default_cooldown          = 300
      asg.desired_capacity          = 3
      asg.health_check_grace_period = 300
      asg.health_check_type         = EC2
      asg.id                        = cs-test
      asg.launch_configuration      = ECS cs-test
      asg.load_balancers            =
      asg.max_size                  = 6
      asg.min_size                  = 3
      asg.name                      = cs-test
      launch_configuration.id       = ECS cs-test
      subnet_ids                    = subnet-55993a0d,subnet-6bb26b41,subnet-70a62e06,subnet-b7fe908a
      vpc_cidr                      = 10.0.0.0/16
      vpc_id                        = vpc-2a43624e[0m
    
    Differential Revision: https://phabricator.blueapron.com/D21

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,15 @@
-output "autoscaling.id" {
-  value = "${aws_autoscaling_group.ecs-cluster.id}"
-}
-
-output "autoscaling.availability_zones" {
-  value = "${aws_autoscaling_group.ecs-cluster.availability_zones}"
-}
-
-output "autoscaling.min_size" {
-  value = "${aws_autoscaling_group.ecs-cluster.min_size}"
-}
-
-output "autoscaling.max_size" {
-  value = "${aws_autoscaling_group.ecs-cluster.max_size}"
-}
-
-output "autoscaling.default_cooldown" {
-  value = "${aws_autoscaling_group.ecs-cluster.default_cooldown}"
-}
-
-output "autoscaling.name" {
-  value = "${aws_autoscaling_group.ecs-cluster.name}"
-}
-
-output "autoscaling.health_check_grace_period" {
-  value = "${aws_autoscaling_group.ecs-cluster.health_check_grace_period}"
-}
-
-output "autoscaling.health_check_type" {
-  value = "${aws_autoscaling_group.ecs-cluster.health_check_type}"
-}
-
-output "autoscaling.desired_capacity" {
-  value = "${aws_autoscaling_group.ecs-cluster.desired_capacity}"
-}
-
-output "autoscaling.launch_configuration" {
-  value = "${aws_autoscaling_group.ecs-cluster.launch_configuration}"
-}
-
-output "autoscaling.vpc_zone_identifier" {
-  value = "${aws_autoscaling_group.ecs-cluster.vpc_zone_identifier}"
-}
-
-output "autoscaling.load_balancers" {
-  value = "${aws_autoscaling_group.ecs-cluster.load_balancers}"
-}
-
-output "launch_configuration.id" {
-  value = "${aws_launch_configuration.ecs.id}"
-}
+output "asg.id" { value = "${aws_autoscaling_group.ecs-cluster.id}" }
+output "asg.availability_zones" { value = "${join(",", aws_autoscaling_group.ecs-cluster.availability_zones)}" }
+output "asg.min_size" { value = "${aws_autoscaling_group.ecs-cluster.min_size}" }
+output "asg.max_size" { value = "${aws_autoscaling_group.ecs-cluster.max_size}" }
+output "asg.default_cooldown" { value = "${aws_autoscaling_group.ecs-cluster.default_cooldown}" }
+output "asg.name" { value = "${aws_autoscaling_group.ecs-cluster.name}" }
+output "asg.health_check_grace_period" { value = "${aws_autoscaling_group.ecs-cluster.health_check_grace_period}" }
+output "asg.health_check_type" { value = "${aws_autoscaling_group.ecs-cluster.health_check_type}" }
+output "asg.desired_capacity" { value = "${aws_autoscaling_group.ecs-cluster.desired_capacity}" }
+output "asg.launch_configuration" { value = "${aws_autoscaling_group.ecs-cluster.launch_configuration}" }
+output "asg.load_balancers" { value = "${join(",", aws_autoscaling_group.ecs-cluster.load_balancers)}" }
+output "launch_configuration.id" { value = "${aws_launch_configuration.ecs.id}" }
+output "vpc_id" { value = "${aws_vpc.ecs.id}" }
+output "vpc_cidr" { value = "${aws_vpc.ecs.cidr_block}" }
+output "subnet_ids" { value = "${join(",", aws_subnet.ecs.*.id)}" }
