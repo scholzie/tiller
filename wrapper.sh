753cf45c3af3c72de93e71c285a5f85f05c6ef08
Chris Scholz <chris.scholz@blueapron.com>
2016-02-29 15:35:32 -0500

    Save terraform.tfstate to s3 secrets bucket

--- wrapper.sh
+++ wrapper.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
 # Description: 
 #   This script is a wrapper for Terraform, ostensibly to manage tfstate, but
-#   mostly just to make your life more difficult.
+#   mostly just to make your life more difficult. Much of it was ripped from 
+#   the Mozilla Socorro project here:
+#       https://github.com/mozilla/socorro-infra
 #
 # Maintainer: DevOps Kr3w <devops@blueapron.com>
-# Author: Chris Scholz <scholzie@blueapron.com> // Don't blame him
+# Author: Chris Scholz <scholzie@blueapron.com> // Don't blame plz
 
 TFVARS="terraform.tfvars"
 BUILD_OPTIONS=(global wms demo basic)
@@ -14,7 +16,7 @@ function help {
     echo "USAGE: ${0} <action> <environment> <build option>"
     echo -n "Valid build options are: "
     local opt 
-    for opt in "${ROLES[@]}"; do
+    for opt in "${BUILD_OPTIONS[@]}"; do
         echo -n "$opt"
     done; echo ""
     exit 1
@@ -24,7 +26,9 @@ function check_element_exists() {
     local elem
     for elem in "${@:2}"; do
         # If the arg is in the array, short circuit with success
-        [[ "$elem" == "$1" ]] && return 0
+        if [[ "$elem" == "$1" ]]; then
+            return 0
+        fi
     done
     # otherwise, bail with error
     return 1
@@ -38,8 +42,12 @@ fi
 
 # Check for help request, lack of arguments, and valid arguments
 check_element_exists "$1" "${HELP[@]}"
-[[ "${1}test" == "test" ]] && help
-[[ "$#" != "3" ]] && help
+if [[ "${1}test" == "test" ]]; then
+    help
+fi
+if [[ "$#" != "3" ]]; then
+    help
+fi
 check_element_exists "$3" "${BUILD_OPTIONS[@]}"
 if [ $? -ne 0 ]; then
     echo "${3} is not a valid role."
@@ -52,20 +60,38 @@ if which terraform > /dev/null; then # Will only produce output on STDERR
     exit 1
 fi
 
+# Get the bucket name from the TFVARs file
+BUCKET=$(grep secret_bucket $TFVARS 2>/dev/null)
+if [[ "$?" != "0" ]]; then 
+    echo "ERROR: Did not get valid secret_bucket name from $TFVARS"
+    exit 1
+else
+    BUCKET=$(echo "$BUCKET" | awk -F\" '{print $2}')
+fi
+
 ACTION="$1"
 ENVIRONMENT="$2"
 BUILD_OPT="$3"
 
+# Fail on errors and be verbose
 set -ex
+# change to $BUILD_OPT directory
+pushd "$BUILD_OPT"
 
 # TODO: Grab tfstate here
+aws s3 sync --exclude="*" --include="terraform.tfstate" "s3://${BUCKET}/tfstate/${ENVIRONMENT}/${BUILD_OPT}/" ./
 
-# Run Terraform
+# Run Terraform, but don't fail if TF fails.
 set +e
 terraform $ACTION -var "environment_name=${ENVIRONMENT}"
 EXIT_CODE=$?
+# Fail on errors again.
 set -e
 
-# TODO: Upload tfstate here
+# Upload tfstate here
+aws s3 sync --exclude="*" --include="terraform.tfstate" ./ "s3://${BUCKET}/tfstate/${ENVIRONMENT}/${BUILD_OPT}/" 
+
+# GTFO
+popd
 
 exit $EXIT_CODE
