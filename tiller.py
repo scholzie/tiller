245864257c6c488a1abaa5f594e9c27f1e2114e3
Chris Scholz <chris.scholz@blueapron.com>
2016-04-05 12:26:54 -0400

    Created abstract and inherited classes
    Summary: .
    
    Test Plan: .
    
    Reviewers: #devops, jack.dwyer, paul
    
    Differential Revision: https://phabricator.blueapron.com/D108

--- tiller.py
+++ tiller.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 """Usage:
-    tiller.py [options] plan <resource> [--destroy]
-    tiller.py [options] build <resource>
-    tiller.py [options] show <resource>
-    tiller.py [options] destroy <resource>
-    tiller.py [options] list
-    tiller.py [options] describe <resource>
-    tiller.py [options] stage
+    tiller.py [options]... plan <resource> [--destroy]
+    tiller.py [options]... build <resource>
+    tiller.py [options]... show <resource>
+    tiller.py [options]... destroy <resource>
+    tiller.py [options]... list
+    tiller.py [options]... describe <resource>
+    tiller.py [options]... stage
 
 Commands:
     plan        Show the plan for the specified resource.
@@ -19,6 +19,7 @@ Commands:
     stage       Experimental. Stage the environment for manual TF run.
 
 Options:
+    --var=<key=value>  key/value pairs to write to configuration
     -h --help          show this screen and exit
     -E ENV, --env=env  target environment
     -v --verbose       be noisy
@@ -32,25 +33,72 @@ import logging
 import os
 import ConfigParser
 import textwrap
+from abc import ABCMeta, abstractmethod
+import json
 
 name = 'tiller'
 version = '0.0.1'
 
+class Packer:
+    """docstring for Packer"""
+    __metaclass__ = ABCMeta
+    def __init__(self):
+        logging.debug("In Packer metaclass")
+        super(Packer, self).__init__()
+    
+    @abstractmethod
+    def build(self):
+        pass
+        
+
+class Terraform:
+    """docstring for Terraform"""
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        logging.debug("In Terraform metaclass")
+        super(Terraform, self).__init__()
+
+    @abstractmethod
+    def plan(self):
+        pass
+
+    @abstractmethod
+    def build(self):
+        pass
+
+    @abstractmethod
+    def show(self):
+        pass
+
+    @abstractmethod
+    def destroy(self):
+        pass
+        
+
 class TillerResource(object):
     """A TillerResource describes an object which tiller can build, destroy, or describe"""
-    def __init__(self, name, path, description=None, long_description=None):
+    def __init__(self, name, path, *args, **kwargs):
+        logging.debug("TillerResource.__init__()")
+        logging.debug("{}:{}\n*args: {}\n**kwargs: {}".format(name, path, args, kwargs))
         super(TillerResource, self).__init__()
         self.name = name
         self.path = path
-        self.description = description
-        self.long_description = long_description
+        self.description = kwargs.get('description')
+        self.long_description = kwargs.get('long_description')
+        self.depends_on = kwargs.get('depends_on')
+        if self.depends_on:
+            self.depends_on = json.loads(self.depends_on)
+        self.required_vars = kwargs.get('required_vars')
+        if self.required_vars:
+            self.required_vars = json.loads(self.required_vars)
         self.__staged = False
 
 
     @classmethod
     def from_config(cls, config_file):
         """Create a TillerResource from a configuration file"""
-        configuration = {}
+        configuration = dict()
         config = ConfigParser.RawConfigParser()
         config.read(config_file)
         section = "resource"
@@ -59,7 +107,7 @@ class TillerResource(object):
         configuration['path'] = os.path.dirname(config_file)
         logging.debug("Resource path: %s" % configuration['path'])
         try:
-            for param in ['name', 'description', 'long_description']:
+            for param in config.options(section):
                 try:
                     configuration[param] = config.get(section, param).strip('"').strip("'")
                 except ConfigParser.NoOptionError:
@@ -74,12 +122,10 @@ class TillerResource(object):
         except:
             raise
 
-        logging.debug("TillerResource created: %r", configuration)
+        return cls(configuration.pop('name'), 
+                   configuration.pop('path'), 
+                   **configuration)
 
-        return cls(configuration['name'],
-                   configuration['path'],
-                   configuration['description'],
-                   configuration['long_description'])
 
     def validate_config(self):
         # TODO: verify path actually exists
@@ -87,19 +133,54 @@ class TillerResource(object):
             self.__config_valid = True
         return self.__config_valid
 
+    @abstractmethod
     def stage(self):
-        """Stages a resource prior to executing a command. 
-        Sets environment, parses configuration files, and stops 
+        """Stages a resource prior to executing a command.
+        Sets environment, parses configuration files, and stops
         just prior to executing requested command"""
-        # TODO: Implement TerraformResource.stage()
         pass
+
 
     def __str__(self):
         return "%s: %s" % (self.name, self.path)
 
-
     def __bool__(self):
         return self.validate_config()
+
+
+class TillerTerraformResource(TillerResource, Terraform):
+    """docstring for TillerTerraformResource"""
+    def __init__(self, name, path, *args, **kwargs):
+        logging.debug("Creating TillerTerraformResource")
+        super(TillerTerraformResource, self).__init__(name, path, *args, **kwargs)
+
+    def plan(self):
+        pass
+
+    def build(self):
+        # build command = self.kwargs['build_cmd']
+        pass
+
+    def show(self):
+        pass
+
+    def destroy(self):
+        pass
+
+
+class TillerPackerResource(TillerResource, Packer):
+    """docstring for TillerPackerResource"""
+    def __init__(self, name, path, *args, **kwargs):
+        logging.debug("Creating TillerPackerResource")
+        super(TillerPackerResource, self).__init__(name, path, *args, **kwargs)
+        self.name = name
+        self.path = path
+        self.args = args
+        self.kwargs = kwargs
+
+    def build(self):
+        pass
+        
 
 def compile_resources():
     resources = {}
@@ -109,10 +190,17 @@ def compile_resources():
     for root, dirs, files in os.walk(resources_dirname):
         for f in files:
             if f == config_filename:
-                logging.debug("creating resource from %s" % f)
-                r = TillerResource.from_config(os.path.join(root, f))
+                logging.debug("creating resource from %s in %s" % (f,root))
+                namespace = root.split('/')[1]
+                if namespace == 'terraform':
+                    r = TillerTerraformResource.from_config(os.path.join(root, f))
+                elif namespace == 'packer':
+                    r = TillerPackerResource.from_config(os.path.join(root,f))
+                else:
+                    r = TillerResource.from_config(os.path.join(root, f))
                 # Create a new entry in resource{} with the name of the resource as the value
                 if r:
+                    r.name = '{}/{}'.format(namespace, r.name)
                     resources[r.name] = r
 
     return resources
@@ -175,9 +263,15 @@ def main(args):
     elif args['list']:
         logging.info("Listing resources.")
         resources = compile_resources()
-        output_lines = []
+        print('The following resources are availble:')
         for r in resources.values():
-            print('\t{:<16}  {}'.format(*[r.name, r.description]))
+            if r.depends_on:
+                print('\t{:<16}\t{} (Depends on {})'.format(*[r.name, r.description, ', '.join(r.depends_on)]))
+            else:
+                print('\t{:<16}\t{}'.format(*[r.name, r.description]))
+
+
+        print('\nFor more information see: ./tiller.py describe <resource>')
 
     elif args['stage']:
         # TODO: Implement 'stage'
@@ -186,4 +280,6 @@ def main(args):
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='%s v%s' % (name, version))
+    if args['--debug']:
+        print args
     main(args)
