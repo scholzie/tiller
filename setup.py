6eae02f94b3285aff3db6423e3d3a4f964f3fa5d
Chris Scholz <chris.scholz@blueapron.com>
2016-04-12 13:18:12 -0400

    config dictionary key ordering

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ except ImportError:
     from distutils.core import setup
 
 config = {
+        'name':         'tiller',
+        'version':      '0.0.1',
         'description':  'Tiller. Sow your oats.',
         'author':       'Chris Scholz',
         'author_email': 'devops@blueapron.com',
-        'version':      '0.0.1',
         'install_requires': ['nose'],
         'packages':     ['tiller'],
-        'scripts':      [],
-        'name':         'tiller'
+        'scripts':      []
         }
 
 setup(**config)
